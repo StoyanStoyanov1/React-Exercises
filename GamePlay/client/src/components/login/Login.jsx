0f5fc031bf5ee8ac608e6b064aeae5f2b423f630
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-04-20 10:34:33 +0200

    update

--- GamePlay/client/src/components/login/Login.jsx
+++ GamePlay/client/src/components/login/Login.jsx
@@ -1,22 +1,44 @@
-export default function Login() {
-    return (
-        <section id="login-page" className="auth">
-            <form id="login">
+import userForm from "../../hooks/userForm.js";
 
-                <div className="container">
-                    <div className="brand-logo"></div>
-                    <h1>Login</h1>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
+export default function Login({loginSubmitHandler}) {
 
-                    <label htmlFor="login-pass">Password:</label>
-                    <input type="password" id="login-password" name="password" />
-                    <input type="submit" className="btn submit" value="Login" />
-                    <p className="field">
-                        <span>If you don't have profile click <a href="#">here</a></span>
-                    </p>
-                </div>
-            </form>
-        </section>
-    );
+	const {values, onChange, onSubmit} = userForm(loginSubmitHandler,
+		{
+			email: '',
+			password: '',
+		}
+	);
+
+	return (
+		<section id="login-page" className="auth">
+			<form id="login" onSubmit={onSubmit}>
+
+				<div className="container">
+					<div className="brand-logo"></div>
+					<h1>Login</h1>
+					<label htmlFor="email">Email:</label>
+					<input
+						type="email"
+						id="email" name="email"
+						placeholder="Sokka@gmail.com"
+						onChange={onChange}
+						value={values.email}
+					/>
+
+					<label htmlFor="login-pass">Password:</label>
+					<input
+						type="password"
+						id="login-password"
+						name="password"
+						onChange={onChange}
+						value={values.password}
+					/>
+					<input type="submit" className="btn submit" value="Login"/>
+					<p className="field">
+						<span>If you don't have profile click <a href="#">here</a></span>
+					</p>
+				</div>
+			</form>
+		</section>
+	);
 }
