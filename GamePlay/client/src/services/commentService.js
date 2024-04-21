48b025ed64bb7e3b17b4bacd3771d511da3841ef
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-04-21 11:34:04 +0200

    add new game and komments

--- GamePlay/client/src/services/commentService.js
+++ GamePlay/client/src/services/commentService.js
@@ -1,6 +1,6 @@
 import * as request from '../lib/request';
 
-const baseUrl = 'http://localhost:3030/jsonstore/comments';
+const baseUrl = 'http://localhost:3030/data/comments';
 
 export const getAll = async (gameId) => {
     const query = new URLSearchParams({
@@ -10,7 +10,7 @@ export const getAll = async (gameId) => {
     const result = await request.get(`${baseUrl}`);
 
     // TODO: temp solution until migration to collections service 
-    return Object.values(result).filter(comment => comment.gameId === gameId);
+    return result.filter(comment => comment.gameId === gameId);
 };
 
 export const create = async (gameId, username, text) => {
--- GamePlay/client/src/services/gameService.js
+++ GamePlay/client/src/services/gameService.js
@@ -1,6 +1,6 @@
 import * as request from "../lib/request";
 
-const baseUrl = 'http://localhost:3030/jsonstore/games'
+const baseUrl = 'http://localhost:3030/data/games'
 
 export const getAll = async () => {
     const result = await request.get(baseUrl);
