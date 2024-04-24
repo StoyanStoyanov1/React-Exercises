a1e7a61cdb1fb79544f91b2e332fa29d82bc739c
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-04-24 08:31:13 +0200

    update

--- GamePlay/client/src/components/gameCreate/gameCreate.jsx
+++ GamePlay/client/src/components/gameCreate/gameCreate.jsx
@@ -1,8 +1,16 @@
 export default function GameCreate() {
+	const createGameSubmitHandler = (e) => {
+		e.preventDefault();
+
+		const gameData = Object.fromEntries(new FormData(e.currentTarget));
+
+		console.log(gameData)
+	}
+
 	return (
 		<section id="create-page" className="auth">
-			<form id="create">
-				<div className="container">
+			<form id="create" onSubmit={createGameSubmitHandler}>
+				<div className="container" >
 
 					<h1>Create Game</h1>
 					<label htmlFor="leg-title">Legendary title:</label>
