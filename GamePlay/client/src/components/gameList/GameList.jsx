614a84582a9bec92842807a2fe4b204793cd9aa2
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-04-24 11:28:22 +0200

    all games

--- GamePlay/client/src/components/gameList/GameList.jsx
+++ GamePlay/client/src/components/gameList/GameList.jsx
@@ -1,35 +1,26 @@
+import {useState, useEffect} from "react";
+
+import * as gameServer from '../../services/gameService.js';
+import GameListItem from "./gameListItem/GameListItem.jsx";
+
 export default function GameList() {
+	const [games, setGames] = useState([]);
+
+	useEffect(() => {
+		gameServer.getAll()
+			.then(result => setGames(result))
+	}, []);
+
 	return (
 		<section id="catalog-page">
 			<h1>All Games</h1>
-			<div className="allGames">
-				<div className="allGames-info">
-					<img src="./images/avatar-1.jpg" />
-						<h6>Action</h6>
-						<h2>Cover Fire</h2>
-						<a href="#" className="details-button">Details</a>
-				</div>
 
-			</div>
-			<div className="allGames">
-				<div className="allGames-info">
-					<img src="./images/avatar-1.jpg" />
-						<h6>Action</h6>
-						<h2>Zombie lang</h2>
-						<a href="#" className="details-button">Details</a>
-				</div>
+			{games.map(game => (
+				<GameListItem key={game._id} {...game}/>
+			))}
 
-			</div>
-			<div className="allGames">
-				<div className="allGames-info">
-					<img src="./images/avatar-1.jpg" />
-						<h6>Action</h6>
-						<h2>MineCraft</h2>
-						<a href="#" className="details-button">Details</a>
-				</div>
-			</div>
+			{games.length === 0 && <h3 className="no-articles">No articles yet</h3>}
 
-			<h3 className="no-articles">No articles yet</h3>
 		</section>
 	)
 
---
+++ GamePlay/client/src/services/gameService.js
@@ -0,0 +1,25 @@
+import request from '../lib/request.js'
+
+const baseUrl = 'http://localhost:3030/jsonstore/games';
+
+
+export const getAll = async () => {
+	const result = await request('GET', baseUrl)
+
+	return Object.values(result)
+
+}
+export const create = async (gameData) => {
+	const response = await fetch(baseUrl, {
+		method: 'POST',
+		headers: {
+			'content-type': 'application/json',
+		},
+		body: JSON.stringify(gameData)
+
+	});
+
+	const result = await response.json();
+
+	return result;
+}
