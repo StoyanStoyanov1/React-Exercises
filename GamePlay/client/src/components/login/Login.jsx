fe882ad5328af82d1b0f41ab3cab872081cd702f
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-04-25 09:25:57 +0200

    create custom hook useForm

--- GamePlay/client/src/components/login/Login.jsx
+++ GamePlay/client/src/components/login/Login.jsx
@@ -1,4 +1,14 @@
+import useForm from "../../hooks/useForm.js";
+
 export default function Login() {
+	const loginForm = {Email: 'email', Password: 'password'};
+
+
+	const [values, onChange, onSubmit] = useForm({
+		[loginForm.Email]: '',
+		[loginForm.Password]: '',
+	});
+
 	return (
 		<section id="login-page" className="auth">
 			<form id="login">
@@ -7,10 +17,26 @@ export default function Login() {
 					<div className="brand-logo"></div>
 					<h1>Login</h1>
 					<label htmlFor="email">Email:</label>
-					<input type="email" id="email" name="email" placeholder="Sokka@gmail.com"/>
+					<input
+						type="email"
+						id="email"
+						name="email"
+						placeholder="Sokka@gmail.com"
+						value={values[loginForm.Email]}
+						onChange={onChange}
+						onSubmit={onSubmit}
+					/>
 
 					<label htmlFor="login-pass">Password:</label>
-					<input type="password" id="login-password" name="password"/>
+					<input
+						type="password"
+						id="login-password"
+						name="password"
+						value={values[loginForm.Password]}
+						onChange={onChange}
+						onSubmit={onSubmit}
+
+					/>
 					<input type="submit" className="btn submit" value="Login"/>
 					<p className="field">
 						<span>If you don't have profile click <a href="#">here</a></span>
