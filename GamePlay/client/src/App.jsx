accc2166eec2520f72e8cfa244e795f0664a904d
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-08 08:17:04 +0200

    server

--- GamePlay/client/src/App.jsx
+++ GamePlay/client/src/App.jsx
@@ -1,9 +1,8 @@
-import {Routes, Route, useNavigate, RouterProvider} from 'react-router-dom';
-import {useState} from "react";
+import {Routes, Route} from 'react-router-dom';
 
-
-import * as authService from './services/authService.js';
 import {AuthProvider} from "./context/authContext.jsx";
+import ErrorBoundary from "./components/ErrorBoundary.jsx";
+import AuthGuard from "./components/guards/AuthGuard.jsx";
 
 import Header from "./components/header/Header.jsx";
 import Home from "./components/home/Home.jsx";
@@ -13,11 +12,10 @@ import Login from "./components/login/Login.jsx";
 import Register from "./components/register/Register.jsx";
 import GameDetails from "./components/gameDetails/GameDetails.jsx";
 import Logout from './components/logout/Logout.jsx'
-import Path from "./paths.js";
 import GameEdit from "./components/gameEdit/GameEdit.jsx";
-import ErrorBoundary from "./components/ErrorBoundary.jsx";
-import BaseAuthGuard from "./components/guards/BaseAuthGuard.jsx";
-import AuthGuard from "./components/guards/AuthGuard.jsx";
+import Path from "./paths.js";
+
+
 
 
 function App() {
