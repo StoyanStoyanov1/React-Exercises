13d4e5086c977412b239993138bbcc7fec4a0672
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-05-15 14:24:57 +0200

    AuthGuard

--- FunFacts/src/App.jsx
+++ FunFacts/src/App.jsx
@@ -15,8 +15,10 @@ import Logout from "./components/logout/Logout.jsx";
 import EditFunFact from "./components/editFunFact/EditFunFact.jsx";
 import RemoveFunFact from "./components/removeFunFact/RemoveFunFact.jsx";
 import ErrorBoundary from "./components/ErrorBoundary.jsx";
+import AuthGuard from "./components/guards/AuthGuard.jsx";
 
 function App() {
+
 	return (
 		<ErrorBoundary>
 			<AuthProvider>
@@ -28,11 +30,13 @@ function App() {
 						<Route path={Path.FunFacts} element={<FunFacts/>}/>
 						<Route path={Path.Register} element={<Register/>}/>
 						<Route path={Path.Login} element={<Login/>}/>
-						<Route path={Path.CreateFunFact} element={<CreateFunFact/>}/>
 						<Route path={`${Path.DetailsFunFact}/:factId`} element={<DetailsFunFact/>}/>
-						<Route path={Path.Logout} element={<Logout/>}/>
-						<Route path={`${Path.EditFunFact}/:factId`} element={<EditFunFact/>}/>
-						<Route path={`${Path.RemovePath}/:factId`} element={<RemoveFunFact/>}/>
+						<Route element={<AuthGuard/>}>
+							<Route path={Path.CreateFunFact} element={<CreateFunFact/>}/>
+							<Route path={Path.Logout} element={<Logout/>}/>
+							<Route path={`${Path.EditFunFact}/:factId`} element={<EditFunFact/>}/>
+							<Route path={`${Path.RemovePath}/:factId`} element={<RemoveFunFact/>}/>
+						</Route>
 					</Routes>
 
 					<Footer/>
--- FunFacts/src/components/guards/AuthGuard.jsx
+++ FunFacts/src/components/guards/AuthGuard.jsx
@@ -1,14 +1,14 @@
 import {useContext} from "react";
 import AuthContext from "../../context/AuthContext.jsx";
-import {Navigate, useNavigate} from "react-router-dom";
+import {Navigate, Outlet, useNavigate} from "react-router-dom";
 import Path from "../../paths.js";
 
-export default function AuthGuards() {
+export default function AuthGuard() {
 	const {isAuthenticated} = useContext(AuthContext)
 
 	if (!isAuthenticated) {
 		return <Navigate to={Path.Login}/>
 	}
 
-	return
+	return <Outlet />
 }
