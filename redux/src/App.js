0e78bb676df05f96eaf97658914c9d6170549103
Stoyan <125189626+Stenlyy@users.noreply.github.com>
2024-11-24 16:57:25 +0100

    counter

---
+++ redux/src/App.js
@@ -0,0 +1,12 @@
+import Counter from "./features/counter/Counter";
+
+function App() {
+  return (
+    <main>
+      <h1>Hello</h1>
+      <Counter />
+      </main>
+  );
+}
+
+export default App;
---
+++ redux/src/features/counter/Counter.jsx
@@ -0,0 +1,12 @@
+import { useSelector, useDispathc } from 'react-redux';
+import { useState } from 'react';
+
+const Counter = () => {
+    return (
+        <section>
+            <p>Hallo</p>
+       </section>
+    )
+}
+
+export default Counter;
