334435d71fb95fa3c397b00a4ecbad2cdeb6fd67
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2024-11-30 11:00:12 +0100

    update table

--- filter/src/pages/Info.jsx
+++ filter/src/pages/Info.jsx
@@ -1,16 +1,15 @@
 import Table from "../components/table/Table";
 import infoData from "../utils/tables/testTable/infoData";
 
+const tableHeaderStyles = {
+    backgroundColor: "#f4f4f4",
+    textAlign: "left",
+    fontWeight: "bold",
+    padding: "10px",
+  };
+
 function Info () {
     
-    const tableHeaderStyles = {
-        backgroundColor: "#fff",
-        textAlign: "left",
-        fontWeight: "bold",
-        padding: "10px",
-      };
-
-
     return (
         <>
             <Table values={infoData} tableHeaderStyles={tableHeaderStyles} />
