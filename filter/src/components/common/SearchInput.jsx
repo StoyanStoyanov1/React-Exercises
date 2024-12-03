5e77f4acc97e1c44cba7e9bf499fbf5a3241da26
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2024-12-03 20:51:46 +0100

    add Mui and tailwind to common components

--- filter/src/components/common/SearchInput.jsx
+++ filter/src/components/common/SearchInput.jsx
@@ -1,28 +1,26 @@
+import React from "react";
+import { TextField, IconButton, Box } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+
 function SearchInput() {
   return (
-    <div className="flex items-center border border-gray-300 rounded-md ">
-      <input
-        type="text"
-        placeholder="Search Product"
-        className="flex-grow border-none outline-none px-3 py-1.5"
+    <Box className="flex items-center border border-gray-300 rounded-md shadow-sm overflow-hidden">
+      <TextField
+        variant="standard"
+        placeholder="Search"
+        InputProps={{
+          disableUnderline: true, 
+        }}
+        className="flex-grow px-3 py-1.5 text-sm"
       />
-      <button className="bg-transparent border-none p-2 cursor-pointer text-gray-500 hover:text-gray-700">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="16"
-          height="16"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-        >
-          <circle cx="11" cy="11" r="8"></circle>
-          <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
-        </svg>
-      </button>
-    </div>
+
+      <IconButton
+        className="p-2 text-gray-500 hover:text-gray-700"
+        aria-label="search"
+      >
+        <SearchIcon fontSize="small" />
+      </IconButton>
+    </Box>
   );
 }
 
