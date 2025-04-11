27f5f50037aec9cd022cd9d0b28413d90769b560
Stoyan Stoyanov <125189626+StoyanStoyanov1@users.noreply.github.com>
2025-04-11 10:09:02 +0200

    start a new project

---
+++ category/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import Categories from "./pages/categories.jsx";
+function App() {
+
+  return (
+    <div>
+      <Categories />
+    </div>
+  )
+}
+
+export default App
